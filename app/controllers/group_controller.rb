5dbc8f047322a6cc06cfa01d3852bd569ff520ec
peterhwang2010 <inhohwang2010@gmail.com>
2015-05-18 03:18:45 -0400

    added features to group controller

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -10,6 +10,19 @@ class GroupController < ApplicationController
 	def new
 	end
 
+	def destroy
+		@user_name = current_user.name
+		@all_groups = current_user.groups
+		@group = Group.where(id: params[:id])
+		@group.first.destroy
+		flash[:notice] = "Deleted group!"
+		render 'index'
+	end
+
+	def profile
+		@current_group = current_user.groups.find_by(group_name: params["group_name"])
+	end
+
 	def create
 		@user_name = current_user.name
 		@all_groups = current_user.groups
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'user/edit'
   post '/group/new', to: 'group#create'
+  get 'group/profile', to: 'group#profile'
 
   get 'user/show'
   resources :group
