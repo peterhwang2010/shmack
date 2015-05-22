d63d1ad4cab3237d28e0ee60f4f499a3f3fe64f4
peterhwang2010 <inhohwang2010@gmail.com>
2015-05-22 11:35:46 -0400

    passing in group id

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -1,8 +1,12 @@
 class MatchController < ApplicationController
+ attr_accessor :group_id
+
   def index
 
     @current_group
   end
+
+
 
   def show
   end
@@ -14,6 +18,7 @@ class MatchController < ApplicationController
   end
 
   def new 
+    binding.pry
     @current_group = Group.find_by_id(params[:id].to_i)
     @current_group.matches.create(group_id: @current_group).save
 
