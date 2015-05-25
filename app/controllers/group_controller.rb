a0f539970d87c561fa69d291ba706909a397a636
Jordan <j.falcon122@gmail.com>
2015-05-25 16:23:37 -0400

    Create groups

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -2,12 +2,14 @@ class GroupController < ApplicationController
 
 
 	def index
+		binding.pry
 		@user_name = current_user.name
 		@all_groups = current_user.groups
 
 	end
 
 	def new
+		binding.pry
 	end
 
 	def destroy
@@ -20,14 +22,23 @@ class GroupController < ApplicationController
 	end
 
 	def profile
-		@current_group = current_user.groups.find_by(group_name: params["group_name"])
-		@current_group_users = @current_group.users
+		# binding.pry
+    @group = Group.all.find_by_group_name(params[:group_name])
+		@current_group_users = @group.users
 	end
 
 	def create
+		@members = []
+		id_array = params[:groups][:users_id]
+		id_array.each {|single_id| @members << User.find_by_id(single_id)}
+		@members.compact!
 		@user_name = current_user.name
 		@all_groups = current_user.groups
 		current_user.groups.create(group_name: params["groups"]["group"]).save
+		current_user.groups.last.users = @members
 		render 'index'
+	end
+
+	def members
 	end
 end
