139de8ea9789097b4cf6629231ce7ade40e17e34
peterhwang2010 <inhohwang2010@gmail.com>
2015-05-25 14:30:01 -0400

    changes around matches

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -8,14 +8,22 @@ class MatchController < ApplicationController
 
 
   def show
+    @current_match = Match.all.find_by_name(params[:match_name])
+    @competition = @current_match.users
     binding.pry
+    render "create"
   end
 
   def create
-    binding.pry
+    @competition = []
+    @competition << current_user
+    @competition << User.all.find_by_id(params[:matches][:member])
     @current_group = Group.find_by_id(params[:matches][:group_id].to_i)
     @current_group.matches.last.update(name: params[:matches][:match_name])
     @current_match = @current_group.matches.last
+    @current_match.users << current_user
+    @current_match.users << User.all.find_by_id(params[:matches][:member])
+    binding.pry
   end
 
   def new 
